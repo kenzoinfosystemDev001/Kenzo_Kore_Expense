f03a1e50bfb02b8ab2f855f664f9f9f1080df461
Sujal Kumar <sujal.kumar@corp.ax-iom.in>
2026-07-31 16:19:08 +0530

    Completely remove ErrorBoundary card wrapper so session restoration card never displays

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AppProvider } from './AppContext'
-import { ErrorBoundary } from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </ErrorBoundary>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>,
 )
